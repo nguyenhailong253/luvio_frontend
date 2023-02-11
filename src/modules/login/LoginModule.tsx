7c421985e69f3c2b453e304c93aaf525ab6893b4
Long Nguyen <nguyenhailong253@gmail.com>
2023-02-11 11:43:55 +1100

    use login template from material UI

--- src/modules/login/LoginModule.tsx
+++ src/modules/login/LoginModule.tsx
@@ -1,7 +1,6 @@
-import { Input } from '@mui/material'
-import Button from '@mui/material/Button'
 import React, { useState } from 'react'
 import { useAppDispatch } from '../../redux/hooks'
+import LoginForm from './components/LoginForm'
 import { useSubmitMutation } from './loginApiSlice'
 import { saveLoginDetails } from './loginSlice'
 
@@ -12,21 +11,20 @@ const LoginModule: React.FunctionComponent = () => {
   const [password, setPassword] = useState('')
   const [submit, { isLoading, isSuccess, error, isError }] = useSubmitMutation()
 
-  const onSubmitClicked = async (): Promise<void> => {
+  const onSubmitClicked = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+    e.preventDefault() // INFO: https://stackoverflow.com/a/50193253/8749888
     const respone = await submit({ username, email, password }).unwrap()
-    console.log('test error', error, isError)
     dispatch(saveLoginDetails({ ...respone }))
   }
 
   return (
-    <div>
-      <Input id='email' type='text' placeholder='Email' value={email} onChange={e => { setEmail(e.target.value) }}/>
-      <Input id='username' type='text' placeholder='Username' value={username} onChange={e => { setUserName(e.target.value) }}/>
-      <Input id='password' type='text' placeholder='Passowrd' value={password} onChange={e => { setPassword(e.target.value) }}/>
-      <Button variant="contained" onClick={onSubmitClicked}>
-        Hello World {username}, {password}
-      </Button>
-    </div>
+    <LoginForm
+      email={email}
+      password={password}
+      setEmail={setEmail}
+      setPassword={setPassword}
+      onSubmitClicked={onSubmitClicked}
+    />
   )
 }
 
---
+++ src/modules/login/components/LoginForm.tsx
@@ -0,0 +1,97 @@
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+import Avatar from '@mui/material/Avatar'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Checkbox from '@mui/material/Checkbox'
+import Container from '@mui/material/Container'
+import CssBaseline from '@mui/material/CssBaseline'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Grid from '@mui/material/Grid'
+import Link from '@mui/material/Link'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import React from 'react'
+import Copyright from './Copyright'
+import type { LoginFormProps } from '../types'
+
+const theme = createTheme()
+
+const LoginForm: React.FunctionComponent<LoginFormProps> = (props) => {
+  const { onSubmitClicked, email, password, setEmail, setPassword } = props
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Log in
+          </Typography>
+          <Box component="form" onSubmit={onSubmitClicked} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={email}
+              onChange={e => { setEmail(e.target.value) }}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={e => { setPassword(e.target.value) }}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Log In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright />
+      </Container>
+    </ThemeProvider>
+  )
+}
+
+export default LoginForm
