d4f4e37ea2380887d5c693b27aac62477320dfce
Long Nguyen <nguyenhailong253@gmail.com>
2023-02-16 20:48:52 +1100

    Add nav bar and redirect to profile list from login

--- src/modules/login/LoginModule.tsx
+++ src/modules/login/LoginModule.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { useAppDispatch } from '../../redux/hooks'
 import { saveLoggedInUserDetails } from '../user/userSlice'
 import LoginForm from './components/LoginForm'
@@ -7,6 +8,7 @@ import type { FormEvent } from '../../common/types'
 
 const LoginModule: React.FunctionComponent = () => {
   const dispatch = useAppDispatch()
+  const navigate = useNavigate()
   const [username, setUserName] = useState<string>('')
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
@@ -19,6 +21,7 @@ const LoginModule: React.FunctionComponent = () => {
       const respone = await login({ username, email, password }).unwrap()
       dispatch(saveLoggedInUserDetails({ ...respone }))
       setErrorMsg('')
+      navigate('/profiles')
     } catch (err: any) {
       if (err.data.detail !== undefined) {
         setErrorMsg(err.data.detail)
