3acda082a1e70c8dea0a92bdc4cc89ef64fdfb4a
Long Nguyen <nguyenhailong253@gmail.com>
2023-02-16 20:26:05 +1100

    Add notfound page and profile folder's skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginModule from './modules/login/LoginModule'
+import NotFound from './modules/notfound/NotFoundModule'
 import SignupModule from './modules/signup/SignupModule'
 import store from './redux/store'
 
@@ -11,9 +12,13 @@ const App: React.FunctionComponent = () => (
       <Routes>
         <Route path="/" element={<LoginModule />}/>
         <Route path="/signup" element={<SignupModule />}/>
+        <Route path='/profiles/create'/>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </Provider>
 )
+
+// INFO: Can also use redirect https://stackoverflow.com/a/72090967/8749888
 
 export default App
